1181ea8f4d998b17c82ff6d4daab109118c2d88f
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2021-09-17 17:13:10 +0400

    finished

---
+++ src/services/users.js
@@ -0,0 +1,63 @@
+const knex = require('../knex');
+const fs = require('fs');
+const USER_TABLE = 'users';
+
+module.exports = {
+  async getById(id) {
+    const item = await knex(USER_TABLE).select('*').where({ id }).first();
+    return item;
+  },
+  async getList() {
+    const item = await knex(USER_TABLE).select('*');
+    return item;
+  },
+  async addItem(item) {
+    return knex(USER_TABLE).insert(item);
+  },
+  async updateItem(id, item) {
+    const oldItem = await knex(USER_TABLE).select('*').where({ id }).first();
+
+    if (item.avatar) {
+      try {
+        fs.unlinkSync(oldItem.avatar);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    const updated = await knex(USER_TABLE)
+      .update({
+        username: item.username || null,
+        email: item.email || null,
+        avatar: item.avatar || null,
+        age: item.age || null,
+        phone: item.phone || null,
+      })
+      .where({ id });
+
+    return updated;
+  },
+
+  async patchItem(id, item) {
+    const oldItem = await knex(USER_TABLE).select('*').where({ id }).first();
+
+    if (item.avatar) {
+      try {
+        fs.unlinkSync(oldItem.avatar);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    const updated = await knex(USER_TABLE).update(item).where({ id });
+    return updated;
+  },
+
+  async removeItem(id) {
+    const item = await knex(USER_TABLE).select('*').where({ id }).first();
+    try {
+      fs.unlinkSync(item.avatar);
+    } catch (err) {
+      console.error(err);
+    }
+    return knex(USER_TABLE).where({ id }).del();
+  },
+};
